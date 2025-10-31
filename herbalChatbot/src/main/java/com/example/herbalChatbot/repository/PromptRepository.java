21a8232cf77220d0147308992d1101fe52c58fca
Shruthi018 <137261316+Shruthi018@users.noreply.github.com>
2025-10-31 11:23:41 +0530

    Delete herbalChatbot directory

--- herbalChatbot/src/main/java/com/example/herbalChatbot/repository/PromptRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.herbalChatbot.repository;
-
-import com.example.herbalChatbot.models.Prompt;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PromptRepository extends JpaRepository<Prompt, Long> {
-}
